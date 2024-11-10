fa609ab5af122a35389004669174b7d956755c3f
LeoChengZhu <leozhucheng@gmail.com>
2024-11-10 11:04:25 -0800

    added timer

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import './App.css';
+import { useState, useEffect } from 'react';
+
 function App() {
+  const [time, setTime] = useState(0); 
+  const [startTime, setStartTime] = useState(null); 
+  useEffect(() => {
+    let interval;
+    setStartTime(Date.now());
+    interval = setInterval(() => {
+      const elapsedTime = Date.now() - startTime; 
+      setTime(elapsedTime / 1000); 
+    }, 100); 
+    return () => clearInterval(interval);
+  }, [startTime]);
+
   return (
     <div>
-      <h1>????</h1>
+      <h1>{time.toFixed(2)}</h1>
     </div>
   );
 }
